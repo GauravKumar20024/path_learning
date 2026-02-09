655180bcad2ff2c2a9090083fa014087bf2ccda3
Gaurav Kumar <gauravkumar20024@gmail.com>
2026-02-09 16:23:23 +0530

    learn-components updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div style={{height:"50px",width:"98%",position:"fixed",bottom:"0px" }}>
+    <div style={{height:"50px",width:"98%",position:"absolute" }}>
       <p style={{textAlign:'center'}}>
         © 2025 Path ak Learning. Built for developers, by developers.
 </p>
     </div>
   )
 }
-
 export default Footer
